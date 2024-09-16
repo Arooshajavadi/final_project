41db36bad22aaefa4178e87c71613edc053bfbb9
Aroshajavadi <aroshajavadi1@gmail.com>
2024-09-16 17:04:22 +0330

    update

--- entity/teacher.py
+++ entity/teacher.py
@@ -1,3 +1,13 @@
 from entity.person import Person
 class Teacher(Person):
-    pass
+    def __init__(self,name,family,username,password,phone,skill):
+        Person.__init__(self,name,family,username,password,phone)
+        self.skill = skill
+
+    @property
+    def skill(self):
+        return self._skill
+
+    @skill.setter
+    def skill(self,skill):
+        self._skill = skill
