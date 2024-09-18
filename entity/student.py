84c0bcbe5b8e925900ef435ac3c90306ce760ff5
Aroshajavadi <aroshajavadi1@gmail.com>
2024-09-18 11:20:16 +0330

    update

--- entity/student.py
+++ entity/student.py
@@ -1,16 +1,17 @@
 from entity.person import Person
 import re
+
+
 class Student(Person):
-    def __init__(self,name,family,username,password,phone,grade):
-        Person.__init__(self,name,family,username,password,phone)
-        self.grade= grade
+    def __init__(self, name, family, username, password, phone, grade):
+        Person.__init__(self, name, family, username, password, phone)
+        self.grade = grade
 
     @property
     def grade(self):
         return self._grade
 
     @grade.setter
-    def grade(self,grade):
-        if re.match(r"^[0-9]{,2}$",grade):
+    def grade(self, grade):
+        if re.match(r"^[0-9]{,2}$", grade):
             self._grade = grade
-
