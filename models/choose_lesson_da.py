8ceb2d571d3a543b94a6d2c9ed94a88474f71028
Arooshajavadi <aroshajavadi1@gmail.com>
2024-10-01 19:52:04 +0330

    Update

--- models/choose_lesson_da.py
+++ models/choose_lesson_da.py
@@ -3,8 +3,12 @@ from models.choose_lesson import Chooselesson
 
 class ChoosingDa:
 
-  def connect():
-    pass
+     def connect(self):
+        self.connection = mysql.connector.connect(database="TEST")
+        self.cursor = self.connection.cursor()
 
-  def disconnect():
-    pass
+    def disconnect(self, commit=True):
+        if commit:
+            self.connection.commit()
+        self.cursor.close()
+        self.connection.close()
