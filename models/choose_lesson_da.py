214ce6163d4c81623e4a89e493a2d506c1364c0f
Arooshajavadi <aroshajavadi1@gmail.com>
2024-10-01 19:56:38 +0330

    Update

--- models/choose_lesson_da.py
+++ models/choose_lesson_da.py
@@ -12,3 +12,21 @@ class ChoosingDa:
             self.connection.commit()
         self.cursor.close()
         self.connection.close()
+
+     def save(self, person):
+        self.connect()
+        self.cursor.execute('INSERT INTO lesson (name, family, username, password, phone) VALUES (%s,%s,%s,%s,%s)',
+                            [person.name, person.family, person.username, person.password, person.phone])
+        self.disconnect(True)
+
+    def edit(self, person):
+        self.connect()
+        self.cursor.execute('UPDATE lesson SET name = %s, family = %s, username = %s, password = %s, phone = %s WHERE id=%s',
+                            [person.name, person.family, person.username, person.password, person.phone])
+        self.disconnect(True)
+
+    def remove(self, id):
+        self.connect()
+        self.cursor.execute('DELETE FROM lesson WHERE id = %s', [id])
+        self.disconnect(True)
+
