48f6e13a509a82251c7133e7a87137be32d821aa
Arooshajavadi <aroshajavadi1@gmail.com>
2024-10-03 22:36:18 +0330

    Update

--- models/choose_lesson_da.py
+++ models/choose_lesson_da.py
@@ -1,30 +1,34 @@
 import mysql.connector
-from models.choose_lesson import Chooselesson
+from models.choose_lesson import ChooseLesson
 
 class ChoosingDa:
 
-     def connect(self):
+    def connect(self):
         self.connection = mysql.connector.connect(database="TEST")
         self.cursor = self.connection.cursor()
 
+    
     def disconnect(self, commit=True):
         if commit:
             self.connection.commit()
         self.cursor.close()
         self.connection.close()
 
-     def save(self, person):
+    
+    def save(self, person):
         self.connect()
-        self.cursor.execute('INSERT INTO lesson (name, family, username, password, phone) VALUES (%s,%s,%s,%s,%s)',
-                            [person.name, person.family, person.username, person.password, person.phone])
+        self.cursor.execute('INSERT INTO lesson (id, username, password, teacher, lesson) VALUES (%s, %s, %s, %s, %s)',
+                             [person.id, person.username, person.password, person.teacher, person.lesson])
         self.disconnect(True)
 
+    
     def edit(self, person):
         self.connect()
-        self.cursor.execute('UPDATE lesson SET name = %s, family = %s, username = %s, password = %s, phone = %s WHERE id=%s',
-                            [person.name, person.family, person.username, person.password, person.phone])
+        self.cursor.execute('UPDATE lesson SET username = %s, password = %s, teacher = %s, lesson = %s WHERE id=%s',
+                            [person.username, person.password, person.teacher, person.lesson])
         self.disconnect(True)
 
+    
     def remove(self, id):
         self.connect()
         self.cursor.execute('DELETE FROM lesson WHERE id = %s', [id])
