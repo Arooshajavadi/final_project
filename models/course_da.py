1ca542146dde36666825d3ed5f9fb446cf3259af
Arooshajavadi <aroshajavadi1@gmail.com>
2024-10-04 18:30:32 +0330

    Update

--- models/course_da.py
+++ models/course_da.py
@@ -5,7 +5,7 @@ from models.course import ChooseLesson
 class ChoosingDa:
 
      def connect(self):
-        self.connection = mysql.connector.connect(database="TEST")
+        self.connection = mysql.connector.connect(database="lesson")
         self.cursor = self.connection.cursor()
 
     def disconnect(self, commit=True):
